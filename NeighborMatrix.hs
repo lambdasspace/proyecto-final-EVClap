9b9e4f60e1995c1e1ce831b7a38b466aed85d305
Emilio <92702301+EVClap@users.noreply.github.com>
2023-06-08 20:12:01 -0700

    Update NeighborMatrix.hs

--- NeighborMatrix.hs
+++ NeighborMatrix.hs
@@ -17,8 +17,9 @@ getValue m row col = case rotate m row col of
 -- Function to get the neighbors of a cell in a matrix
 getNeighbors :: Matrix a -> Int -> Int -> Neighbors a
 getNeighbors m row col = (at 0 1, at 0 2, at 1 2, at 2 2, at 2 1, at 2 0, at 1 0, at 0 0)
- where at = getValue rotMat
- rotMat = rotate m (row-1) (col-1) -- list of comprehension used to create lists in a way that's similar to set notation in math
+      where at = getValue rotMat
+            rotMat = rotate m (row-1) (col-1)    -- list of comprehension used to create lists in a way that's similar to set notation in math
+
 
 -- Function to rotate a matrix by a certain number of rows and columns
 rotate :: Matrix a -> Int -> Int -> Matrix a
@@ -27,7 +28,7 @@ rotate m rows cols = rotateHorizontal (rotateVertical m cols) rows
 -- Function to rotate a matrix vertically
 rotateVertical :: Matrix a -> Int -> Matrix a
 rotateVertical m n | n>=0 = iterate rotateVerticalOnceLeft m !! n
- | otherwise = iterate rotateVerticalOnceRight m !! (-n)
+      | otherwise = iterate rotateVerticalOnceRight m !! (-n)
 
 -- Function to rotate a matrix vertically to the left
 rotateVerticalOnceLeft :: Matrix a -> Matrix a
@@ -49,7 +50,7 @@ rotateRowOnceLeft (x:xs) = xs++[x]
 -- Function to rotate a matrix horizontally
 rotateHorizontal :: Matrix a -> Int -> Matrix a
 rotateHorizontal m n | n>=0 = iterate rotateRowOnceLeft m !! n
- | otherwise = iterate rotateRowOnceRight m !! (-n)
+      | otherwise = iterate rotateRowOnceRight m !! (-n)
 
 -- Function to get a matrix of neighbors for each cell in a matrix
 getNeighborMatrix :: Matrix a -> Matrix (Neighbors a)
@@ -57,8 +58,10 @@ getNeighborMatrix m = [ [ getNeighbors m y x | x<-[0..((length (m!!y))-1)] ] | y
 
 -- Function to map a function over a matrix, passing each cell and its neighbors to the function
 mapWithValueNeighbors :: (a -> Neighbors a -> b) -> Matrix a -> Matrix b
-mapWithValueNeighbors f m = [ [ f (m!!y!!x) (neighborMatrix!!y!!x)| x<-[0..((length (neighborMatrix!!y)-1))]]| y<-[0..((length neighborMatrix)-1)]] where neighborMatrix = getNeighborMatrix m
+mapWithValueNeighbors f m = [ [ f (m!!y!!x) (neighborMatrix!!y!!x)| x<-[0..((length (neighborMatrix!!y)-1))]]| y<-[0..((length neighborMatrix)-1)]] 
+      where neighborMatrix = getNeighborMatrix m
 
 -- Function to map a function over a matrix, passing the neighbors of each cell to the function
 mapWithNeighbors :: (Neighbors a -> b) -> Matrix a -> Matrix b
-mapWithNeighbors f m = [ [ f (neighborMatrix!!y!!x)| x<-[0..((length (neighborMatrix!!y)-1))]]| y<-[0..((lengthneighborMatrix)-1)]] where neighborMatrix = getNeighborMatrix m
+mapWithNeighbors f m = [ [ f (neighborMatrix!!y!!x)| x<-[0..((length (neighborMatrix!!y)-1))]]| y<-[0..((length neighborMatrix)-1)]] 
+      where neighborMatrix = getNeighborMatrix m
